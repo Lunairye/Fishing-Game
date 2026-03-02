5ce45b2249242bc1a56787f0b7eda0b1e8a1bea7
Lunairye <waowoaw@outlook.com>
2026-03-02 23:46:35 +0000

    Hook Rotation and Fishing Line Fixed

--- Fishing Rod/fishing_hook_manager.gd
+++ Fishing Rod/fishing_hook_manager.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 
-var state = ""
+var hook_state = ""
 var reeled = "reeled"
 var thrown = "thrown"
 var sinking = "sinking"
+var reeling = "reeling"
 
 var launch_angle = PI/6
 var launch_vector = Vector2.ZERO
@@ -11,31 +12,41 @@ var travel_vector = Vector2.ZERO
 
 var line_length = 100
 
+@onready var reeled_marker = self.find_parent("Fishing Rod").find_child("Fishing Hook Reeled Marker")
+var centre_to_edge_vector = Vector2.ZERO
+var tangent_vector = Vector2.ZERO
+var override_velocity_max_length = false
+
+@onready var hook_sprite = $"Hook Sprite"
+@onready var hook_current_marker = $"Hook Sprite/Fishing Hook Current Mark"
+
 func _ready() -> void:
-	state = reeled
-	self.rotate(PI/2)
+	hook_state = reeled
+	
+	
 
 
 func _physics_process(delta: float) -> void:
-	if state == reeled:
-		self.gravity_scale = 0
-	elif state == thrown:
-		self.gravity_scale = 1
-		travel_vector = self.linear_velocity.normalized()
-		look_at(travel_vector)
-		
-	elif state == sinking:
+	if hook_state == reeled:
+		gravity_scale = 0
+	else:
+		gravity_scale = 1
+	
+	if hook_state == sinking:
 		print("sinking")
 	
-	
-	
-	
+	if hook_state == thrown or hook_state == sinking:
+		hook_sprite.rotation = to_local(hook_current_marker.global_position + linear_velocity).angle() + PI/2
+	else:
+		hook_sprite.rotation = 0
 
 func _launch(power: int) -> void:
 	launch_vector.x = cos(launch_angle)
 	launch_vector.y = -sin(launch_angle)
-	launch_vector.normalized()
+	launch_vector = launch_vector.normalized()
 	
-	self.apply_impulse(launch_vector * power)
-	state = thrown
-	
+	apply_impulse(launch_vector * power)
+	hook_state = thrown
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	pass
--- Fishing Rod/fishing_rod_manager.gd
+++ Fishing Rod/fishing_rod_manager.gd
@@ -9,21 +9,24 @@ var power_scale_speed = 1
 @onready var fishing_hook = $"Fishing Hook"
 @onready var fishing_line = $"Fishing Line"
 @onready var fishing_hook_reeled_marker = $"Fishing Hook Reeled Marker"
-@onready var fishing_hook_current_marker = $"Fishing Hook/Fishing Hook Current Mark"
+@onready var fishing_hook_current_marker = $"Fishing Hook/Hook Sprite/Fishing Hook Current Mark"
 
 func _ready() -> void:
-	fishing_line.add_point(fishing_hook_reeled_marker.position, 0)
-	fishing_line.add_point(fishing_hook.position + fishing_hook_current_marker.position, 1)
+	pass
 
 func _input(event: InputEvent) -> void:
-	while event.is_action_pressed("Fish"):
-		await get_tree().create_timer(power_scale_speed).timeout
-		power += power_scale_amount
+	if fishing_hook.hook_state == "reeled":
+		while event.is_action_pressed("Fish"):
+			await get_tree().create_timer(power_scale_speed).timeout
+			power += power_scale_amount
 		
-	if event.is_action_released("Fish"):
-		fishing_hook._launch(power)
-		power = starting_power
+		if event.is_action_released("Fish"):
+			fishing_hook._launch(power)
+			power = starting_power
+	
+	if fishing_hook.hook_state == "thrown" or fishing_hook.hook_state == "sinking":
+		pass
 	
 func _physics_process(delta: float) -> void:
-	fishing_line.set_point_position(0, fishing_hook_reeled_marker.position)
-	fishing_line.set_point_position(1, fishing_hook.position + fishing_hook_current_marker.position)
+	fishing_line.set_point_position(0, to_local(fishing_hook_reeled_marker.global_position))
+	fishing_line.set_point_position(1, to_local(fishing_hook_current_marker.global_position))
