312f57649873fc1f9bc3059519198b92a4b47aa1
GeorgeHyn <waowoaw@outlook.com>
2026-03-03 16:43:32 +0000

    Added Line Length Limit and Reeling
    This commit also contains changes to the following:
    - Hook Rotation

--- Fishing Rod/fishing_hook_manager.gd
+++ Fishing Rod/fishing_hook_manager.gd
@@ -6,27 +6,42 @@ var thrown = "thrown"
 var sinking = "sinking"
 var reeling = "reeling"
 
+var reel_force
+
 var launch_angle = PI/6
 var launch_vector = Vector2.ZERO
 var travel_vector = Vector2.ZERO
 
 var line_length = 100
 
-@onready var reeled_marker = self.find_parent("Fishing Rod").find_child("Fishing Hook Reeled Marker")
-var centre_to_edge_vector = Vector2.ZERO
-var tangent_vector = Vector2.ZERO
-var override_velocity_max_length = false
+@onready var fishing_rod = self.find_parent("Fishing Rod")
+@onready var reeled_marker = fishing_rod.find_child("Fishing Hook Reeled Marker")
+var hook_to_centre = Vector2.ZERO
 
 @onready var hook_sprite = $"Hook Sprite"
 @onready var hook_current_marker = $"Hook Sprite/Fishing Hook Current Mark"
+@onready var test_line = $Line2D
 
 func _ready() -> void:
 	hook_state = reeled
-	
-	
 
 
 func _physics_process(delta: float) -> void:
+	### Testing Stuff
+	
+	if fishing_rod.reeling:
+		freeze = true
+		global_position += -hook_to_centre.normalized() * fishing_rod.reel_force 
+		hook_sprite.rotation = to_local(hook_current_marker.global_position + linear_velocity).angle() - PI/2
+		
+		if hook_current_marker.global_position.distance_to(reeled_marker.global_position) <= 5:
+			hook_state = reeled
+			global_position = reeled_marker.global_position
+			freeze = false
+	else:
+		freeze = false 
+	
+	### State Machine
 	if hook_state == reeled:
 		gravity_scale = 0
 	else:
@@ -36,7 +51,7 @@ func _physics_process(delta: float) -> void:
 		print("sinking")
 	
 	if hook_state == thrown or hook_state == sinking:
-		hook_sprite.rotation = to_local(hook_current_marker.global_position + linear_velocity).angle() + PI/2
+		hook_sprite.rotation = to_local(hook_current_marker.global_position + linear_velocity).angle() - PI/2 
 	else:
 		hook_sprite.rotation = 0
 
@@ -49,4 +64,7 @@ func _launch(power: int) -> void:
 	hook_state = thrown
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	pass
+	hook_to_centre = hook_current_marker.global_position - reeled_marker.global_position
+	
+	if hook_to_centre.length() >= line_length:
+		global_position = reeled_marker.global_position + hook_to_centre.limit_length(line_length)
--- Fishing Rod/fishing_rod_manager.gd
+++ Fishing Rod/fishing_rod_manager.gd
@@ -6,6 +6,11 @@ var power_max = 50
 var power_scale_amount = 10
 var power_scale_speed = 1
 
+var charging = true
+var reeling = false
+
+var reel_force = 1
+var reel_speed = 0.2
 @onready var fishing_hook = $"Fishing Hook"
 @onready var fishing_line = $"Fishing Line"
 @onready var fishing_hook_reeled_marker = $"Fishing Hook Reeled Marker"
@@ -16,17 +21,31 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if fishing_hook.hook_state == "reeled":
-		while event.is_action_pressed("Fish"):
-			await get_tree().create_timer(power_scale_speed).timeout
-			power += power_scale_amount
+		if event.is_action_pressed("Fish"):
+			charging = true
+			while charging:
+				print(power)
+				await get_tree().create_timer(power_scale_speed).timeout
+				power += power_scale_amount
+				
+				if power >= power_max:
+					power = power_max
 		
-		if event.is_action_released("Fish"):
+		if event.is_action_released("Fish") && charging:
+			charging = false
 			fishing_hook._launch(power)
 			power = starting_power
 	
-	if fishing_hook.hook_state == "thrown" or fishing_hook.hook_state == "sinking":
-		pass
+	
 	
 func _physics_process(delta: float) -> void:
+	
+	if fishing_hook.hook_state == "thrown" or fishing_hook.hook_state == "sinking":
+		if Input.is_action_just_pressed("Fish"):
+			reeling = true
+		
+		if Input.is_action_just_released("Fish"):
+			reeling = false
+	
 	fishing_line.set_point_position(0, to_local(fishing_hook_reeled_marker.global_position))
 	fishing_line.set_point_position(1, to_local(fishing_hook_current_marker.global_position))
